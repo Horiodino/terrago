a328abeab3a1a5bb5966ef99addec2773bbc66f7
horiodino <holiodin@gmail.com>
2023-04-09 18:05:49 +0530

    added-flags

--- azure/iac.go
+++ azure/iac.go
@@ -14,7 +14,7 @@ var (
 	rbac          bool
 )
 
-var k8sCmd = &cobra.Command{
+var cluster = &cobra.Command{
 	Use:   "k8s",
 	Short: "Kubernetes commands",
 	Long:  `Kubernetes commands`,
@@ -25,23 +25,23 @@ var k8sCmd = &cobra.Command{
 }
 
 func init() {
-	k8sCmd.Flags().StringVar(&clusterName, "name", "", "Cluster name")
-	k8sCmd.MarkFlagRequired("name")
-	k8sCmd.Flags().StringVar(&resourceGroup, "rg", "", "Resource group name")
-	k8sCmd.Flags().StringVar(&location, "location", "", "Location for the cluster")
-	k8sCmd.MarkFlagRequired("location")
-	k8sCmd.Flags().StringVar(&os_type, "os_type", "", "OS type for the cluster")
-	k8sCmd.MarkFlagRequired("os_type")
-	k8sCmd.Flags().StringVar(&dnsprefix, "dns_prefix", "", "DNS prefix for the cluster")
-	k8sCmd.MarkFlagRequired("dns_prefix")
-	k8sCmd.Flags().IntVar(&nodepool, "nodepool", 0, "Number of nodes in the cluster")
-	k8sCmd.MarkFlagRequired("nodepool")
-	k8sCmd.Flags().BoolVar(&rbac, "rbac", false, "Enable RBAC")
-	k8sCmd.MarkFlagRequired("rbac")
+	cluster.Flags().StringVar(&clusterName, "name", "", "Cluster name")
+	cluster.MarkFlagRequired("name")
+	cluster.Flags().StringVar(&resourceGroup, "rg", "", "Resource group name")
+	cluster.Flags().StringVar(&location, "location", "", "Location for the cluster")
+	cluster.MarkFlagRequired("location")
+	cluster.Flags().StringVar(&os_type, "os_type", "", "OS type for the cluster")
+	cluster.MarkFlagRequired("os_type")
+	cluster.Flags().StringVar(&dnsprefix, "dns_prefix", "", "DNS prefix for the cluster")
+	cluster.MarkFlagRequired("dns_prefix")
+	cluster.Flags().IntVar(&nodepool, "nodepool", 0, "Number of nodes in the cluster")
+	cluster.MarkFlagRequired("nodepool")
+	cluster.Flags().BoolVar(&rbac, "rbac", false, "Enable RBAC")
+	cluster.MarkFlagRequired("rbac")
 }
 
 func main() {
 	rootCmd := &cobra.Command{Use: "main"}
-	rootCmd.AddCommand(k8sCmd)
+	rootCmd.AddCommand(cluster)
 	rootCmd.Execute()
 }
--- cmd/root.go
+++ cmd/root.go
@@ -1,72 +1,27 @@
-package main
+package cmd
 
 import (
-	"bytes"
-	"fmt"
-	"log"
 	"os"
-	"text/template"
 
 	"github.com/spf13/cobra"
 )
 
-var (
-	rg_name string
-)
+// rootCmd represents the base command when called without any subcommands
+var rootCmd = &cobra.Command{
+	Use:   "terrago",
+	Short: "a CLI tool for Terraform",
+	Long:  "A CLI tool for Terraform that allows you to create, update, and destroy Terraform configurations in a command. ",
+}
 
-// rgname represents the rgname command
-var rgname = &cobra.Command{
-	Use:  "rgname",
-	Long: `rgname is a CLI tool for creating an Azure Resource Group in Terraform`,
-
-	Run: func(cmd *cobra.Command, args []string) {
-		t := `resource "azurerm_resource_group" "{{.RgName}}" {
-			name     = "{{.RgName}}"
-			location = "eastus"
-		}`
-		temp, err := template.New("template").Parse(t)
-		if err != nil {
-			log.Fatal(err)
-		}
-		//define the data to be passed to the template
-		data := struct {
-			RGname string
-		}{
-			RGname: rg_name,
-		}
-
-		//create a new buffer and write the template to it
-		var buf bytes.Buffer
-		if err := temp.Execute(&buf, data); err != nil {
-			fmt.Println(err)
-			os.Exit(1)
-		}
-
-		file, err := os.Create("main.tf")
-		if err != nil {
-			log.Fatal(err)
-		}
-		defer file.Close()
-
-		if _, err := buf.WriteTo(file); err != nil {
-			log.Fatal(err)
-		}
-
-		fmt.Println("Terraform template created")
-	},
+// Execute adds all child commands to the root command and sets flags appropriately.
+// This is called by main.main(). It only needs to happen once to the rootCmd.
+func Execute() {
+	err := rootCmd.Execute()
+	if err != nil {
+		os.Exit(1)
+	}
 }
 
 func init() {
-	rgname.PersistentFlags().StringVarP(&rg_name, "rgname", "n", "", "Resource Group Name")
-	//here we are making the flag required for the command to run successfully
-	rgname.MarkPersistentFlagRequired("rgname")
-}
-
-func main() {
-	rootCmd := &cobra.Command{Use: "terrago"}
-	rootCmd.AddCommand(rgname)
-	if err := rootCmd.Execute(); err != nil {
-		fmt.Println(err)
-		os.Exit(1)
-	}
+	rootCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
 }
