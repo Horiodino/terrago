909f7f66e141747a95f7b8bc30657de3b01f19b5
horiodino <holiodin@gmail.com>
2023-04-09 14:24:33 +0530

    az-vm-configured

--- azure/azurermVM.go
+++ azure/azurermVM.go
@@ -1,18 +1,20 @@
 package azure
 
 import (
+	"fmt"
+
 	"github.com/spf13/cobra"
 )
 
 var (
-	name      string
-	rgname    string
-	location  string
-	os_type   string
-	size      string
-	disk_size int
-	username  string
-	password  string
+	vm_name      string
+	vm_rgname    string
+	vm_location  string
+	vm_os_type   string
+	vm_size      string
+	vm_disk_size int
+	vm_username  string
+	vm_password  string
 )
 
 var vmcmd = &cobra.Command{
@@ -20,10 +22,21 @@ var vmcmd = &cobra.Command{
 	Short: "Create a VM",
 	Long:  `Create a VM`,
 	Run: func(cmd *cobra.Command, args []string) {
-		vmcreate(name, rgname, location, os_type, size, disk_size, username, password)
+		vmcreate(vm_name, vm_rgname, vm_location, vm_os_type, vm_size, vm_disk_size, vm_username, vm_password)
 	},
 }
 
 func vmcreate(name string, rgname string, location string, os_type string, size string, disk_size int, username string, password string) {
+
+	fmt.Println("Creating VM: ", name, " in resource group: ", rgname, "size")
+
+	t := `resource "azurerm_virtual_machine" "example" {
+		name                  = "{{.name}}"
+		location              = "{{.location}}"
+		resource_group_name   = "{{.rgname}}"
+		network_interface_ids = [azurerm_network_interface.example.id]
+		vm_size               = "{{.size}}"
+		os_profile            = "{{.os_type}}"
+		`
 
 }
