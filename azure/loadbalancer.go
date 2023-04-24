c3d4f9811475015538ecd38cb9be3d908a05d839
horiodino <holiodin@gmail.com>
2023-04-24 07:13:24 +0530

    creating loadbalancer ;)

--- azure/loadbalancer.go
+++ azure/loadbalancer.go
@@ -1,10 +1,28 @@
 package azure
 
 import (
-	"context"
-	"fmt"
+	"github.com/Azure/azure-sdk-for-go/services/network/mgmt/2019-09-01/network"
+	// "github.com/Azure/go-autorest/autorest"
+	"github.com/Azure/go-autorest/autorest/azure/auth"
+)
+
+var (
+	loadBalancerName = "loadBalancerName"
+	subscriptionID   = "subscriptionID"
 )
 
 // defining the load balancer
 func loadbalancer() {
+	// authenticating to the Azure API
+	authorizer, err := auth.NewAuthorizerFromEnvironment()
+	if err != nil {
+		panic(err)
+	}
+
+	// creating the client for load balancer
+	lbclient := network.NewLoadBalancersClient(subscriptionID)
+	lbclient.Authorizer = authorizer
+
+	// creating the load balancer
+
 }
