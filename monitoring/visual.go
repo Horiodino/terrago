f36798f92be566a50ad0d5d485650b076598cadc
horiodino <holiodin@gmail.com>
2023-05-01 08:29:49 +0530

    adding visual way to see the structure of your kubernetes deployments

--- monitoring/visual.go
+++ monitoring/visual.go
@@ -16,6 +16,28 @@
 
 // get the info regarding the nodes
 // we already have the info regarding the nodes
+package monitoring
+
+import (
+	"context"
+	"log"
+
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/client-go/kubernetes"
+	"k8s.io/client-go/rest"
+)
+
+// slice for deployment info
+
+var deploymentInfoSlice []deploymentInfo
+
+type deploymentInfo struct {
+	deploymentName string
+	namespace      string
+	replicas       int32
+	labels         map[string]string
+	annotations    map[string]string
+}
 
 func nodeVisuals() {
 	// we will get the info regarding the nodes
@@ -47,4 +69,103 @@ func nodeVisuals() {
 		log.Fatal(err)
 	}
 
+	// now we will get inside the deployments and we will get the info regarding the deployments
+	// we will get the info regarding the deployments and we will store them in a slice
+
+	// now get the info regarding the deployments and store them in a slice
+	for _, deployment := range deployments.Items {
+		deplname := deployment.Name
+		deplns := deployment.Namespace
+		deplreplica := deployment.Spec.Replicas
+		depllable := deployment.Labels
+		deplannotation := deployment.Annotations
+
+		// now we will store the info regarding the deployments in a slice
+		// we will store the info in the deploymentInfo struct
+
+		// now  append the info regarding the deployments in the deploymentInfo struct
+		deploymentInfo := deploymentInfo{
+			deploymentName: deplname,
+			namespace:      deplns,
+			replicas:       *deplreplica,
+			labels:         depllable,
+			annotations:    deplannotation,
+		}
+
+		// now we will append the info regarding the deployments in the deploymentInfo slice
+		deploymentInfoSlice = append(deploymentInfoSlice, deploymentInfo)
+
+	}
+}
+
+var serviceInfoSlice []serviceInfo
+
+type serviceInfo struct {
+	serviceName string
+	namespace   string
+	labels      map[string]string
+	annotations map[string]string
+}
+
+func serviceVisuals() {
+
+	// now we will get the info regarding the services
+	// kuebrnetes client
+
+	// create the kubernetes client
+	config, err := rest.InClusterConfig()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	clientset, err := kubernetes.NewForConfig(config)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// get the services of the cluster
+	services, err := clientset.CoreV1().Services("").List(context.Background(), metav1.ListOptions{})
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// now we will get inside the services and we will get the info regarding the services
+	// we will get the info regarding the services and we will store them in a slice
+
+	// now get the info regarding the services and store them in a slice
+	for _, service := range services.Items {
+		servicename := service.Name
+		servicens := service.Namespace
+		servicelable := service.Labels
+		serviceannotation := service.Annotations
+
+		// now we will store the info regarding the services in a slice
+		// we will store the info in the serviceInfo struct
+
+		// now  append the info regarding the services in the serviceInfo struct
+		serviceInfo := serviceInfo{
+			serviceName: servicename,
+			namespace:   servicens,
+			labels:      servicelable,
+			annotations: serviceannotation,
+		}
+
+		// now we will append the info regarding the services in the serviceInfo slice
+		serviceInfoSlice = append(serviceInfoSlice, serviceInfo)
+	}
+}
+
+var podInfoSlice []podInfo
+
+type podInfo struct {
+	podName      string
+	namespace    string
+	labels       map[string]string
+	annotations  map[string]string
+	replias      int32
+	containers   []string
+	containerImg []string
+}
+
+func podVisuals() {
 }
