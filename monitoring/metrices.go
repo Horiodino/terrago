0bfd161ddca8c2b0f06cf544825fd4fe9397ea04
horiodino <holiodin@gmail.com>
2023-05-03 13:41:37 +0530

    adding the cluster metrices into database with periodic intervals

--- monitoring/metrices.go
+++ monitoring/metrices.go
@@ -487,7 +487,6 @@ func insertDataMongo(resourcesList []resources) error {
 			log.Fatal(err)
 		}
 	}
-
 	return nil
 }
 
@@ -495,6 +494,13 @@ func insertDataMongo(resourcesList []resources) error {
 
 // before that lets get the namespace name from the resources struct
 // namepace struct
+
+// there is a little problem with the namespace struct
+// we created the struct for namespace but the problem is that we are just getting one namespace
+// we need to get the info regarding all the namespaces and inside that we need to get the info regarding the pods etc
+// so we will create a slice of the namespace struct
+var ns []namespace
+
 type namespace struct {
 	namespaceName            string
 	nspods                   []string
@@ -601,6 +607,25 @@ func namespacesInfo(r *resources, ns []namespace) ([]namespace, error) {
 		ns = append(ns, newNS)
 	}
 	return ns, nil
+}
+
+// now we will save the ns slice to the database
+func saveNamespacesInfo(ns []namespace) error {
+
+	// connect to the database
+	client, err := mongo.NewClient(options.Client().ApplyURI("mongodb://localhost:27017"))
+	if err != nil {
+		log.Fatal(err)
+	}
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	defer cancel()
+	err = client.Connect(ctx)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// get the database and the collection
+
 }
 
 // ----------------------------------------------------------------------------------------------------------------------------
