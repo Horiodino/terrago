e3e2b3b40b7f46dbb4b17f1cce61cef19c0eb6c0
Horiodino <holiodin@gmail.com>
2023-06-12 07:58:23 +0530

    adding web interface pkg ...

--- main.go
+++ main.go
@@ -1,13 +1,15 @@
 package main
 
 import (
-	alerts "github.com/Horiodino/terrago/Alerts"
+	// alerts "github.com/Horiodino/terrago/Alerts"
 	// metrices "github.com/Horiodino/terrago/monitoring"
 	// container "github.com/Horiodino/terrago/monitoring/container_metrices"
+	localhost "github.com/Horiodino/terrago/localhost"
 )
 
 func main() {
 
-	alerts.DeploumentsFailure()
+	// alerts.DeploumentsFailure()
+	localhost.host()
 
 }
