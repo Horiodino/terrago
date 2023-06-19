7add4870c2ca1aa018ec035958b8893b1171c9ce
Horiodino <holiodin@gmail.com>
2023-06-19 17:52:33 +0530

    inbounde data and outbound data tracking per package ...

--- network/incoming.go
+++ network/incoming.go
@@ -1,5 +1,95 @@
 package network
 
+import (
+	"fmt"
+	"log"
+
+	"net"
+
+	"github.com/google/gopacket"
+	"github.com/google/gopacket/pcap"
+)
+
+// how its going to work ?
+// we will use hostnetwork : true  so we will containerize the code and run it on the host network which will
+// be node network. so we will be able to access the node network from the container.
+// and we will make a requeste to another pod that will be acessing the node network-info and display it on the browser.
+
+// for now lets code the network part for getting all the possible network info from the node network interface card
+
+func Nic_info() {
+	Nicinfo, err := net.Interfaces()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Println(Nicinfo)
+
+}
+
+var (
+	Total_Data_Rcvd = 0
+	Total_Data_Sent = 0
+)
+
+// per node
 func IncomingTraffic() {
+
+	iface := "wlo1"
+	handle, err := pcap.OpenLive(iface, 1600, true, pcap.BlockForever)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer handle.Close()
+
+	filter := "inbound"
+	err = handle.SetBPFFilter(filter)
+	if err != nil {
+		log.Fatal(err)
+	}
+	Total_Incoming_Packets := 0
+	packetSource := gopacket.NewPacketSource(handle, handle.LinkType())
+
+	for packet := range packetSource.Packets() {
+		// Process each captured packet here
+		Total_Incoming_Packets++
+		fmt.Println(Total_Incoming_Packets)
+		fmt.Println(packet)
+		Total_Data_Rcvd += packet.Metadata().CaptureInfo.Length
+		fmt.Println("Total Data Rcvd:", Total_Data_Rcvd/1024, "KB")
+
+	}
+	fmt.Println("Total packets arrived:", Total_Incoming_Packets)
+}
+
+func Outbound_Traffic() {
+
+	iface := "wlo1"
+	handle, err := pcap.OpenLive(iface, 1600, true, pcap.BlockForever)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	filter := "outbound"
+	err = handle.SetBPFFilter(filter)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	Total_Outgoing_Packets := 0
+
+	packetSource := gopacket.NewPacketSource(handle, handle.LinkType())
+
+	for packet := range packetSource.Packets() {
+		// Process each captured packet here
+		Total_Outgoing_Packets++
+		fmt.Println(Total_Outgoing_Packets)
+		fmt.Println(packet)
+		Total_Data_Sent += packet.Metadata().CaptureInfo.Length
+		fmt.Println("Total Data Sent:", Total_Data_Sent/1024, "KB")
+
+	}
+
+	fmt.Println("Total packets arrived:", Total_Outgoing_Packets)
 
 }
