596d31ab898a01f4cff61798c3b53ee8df90c64f
Horiodino <holiodin@gmail.com>
2023-06-27 20:07:11 +0530

    jwt authentication based login

--- Database/jwt/dbconnection.go
+++ Database/jwt/dbconnection.go
@@ -1,1 +1,37 @@
+package jwt
 
+import (
+	"context"
+	"fmt"
+	"log"
+	"os"
+	"time"
+
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+	"golang.org/x/tools/go/analysis/passes/defers"
+)
+
+func DBinstance() *mongo.Client {
+
+	err := godotenv.Load(".env")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	Mongodb := os.Getenv(MongoDB_URL)
+
+	client, err := mongo.NewClient(options.Client().ApplyURI(Mongodb))
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	defer cancel()
+
+	err = client.Connect(ctx)
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println("Connection established")
+}
