2606b7bef00fa9843a601c52b15af95e38acdac4
Horiodino <holiodin@gmail.com>
2023-07-29 20:30:32 +0530

    added :nodeinfo

--- cmd/web.go
+++ cmd/web.go
@@ -10,6 +10,8 @@ import (
 	"github.com/spf13/cobra"
 )
 
+var displayedNodes = make(map[string]bool)
+var NISLICE = metrices.NodeInfoList
 var allData AllData
 
 var WebCmd = &cobra.Command{
@@ -17,6 +19,7 @@ var WebCmd = &cobra.Command{
 	Short: "Print cluster information",
 	Run: func(cmd *cobra.Command, args []string) {
 		metrices.Getinfo()
+		metrices.Cpu()
 		allData = AllData{
 			Monitoring: metrices.Monitoring{
 				ClusterName: metrices.M.ClusterName,
@@ -29,12 +32,13 @@ var WebCmd = &cobra.Command{
 				Totaldisk:   metrices.M.Totaldisk,
 				Billing:     metrices.M.Billing,
 			},
-			NodeInfoList: []metrices.NodeInfo{},
+			NodeInfoList: NISLICE,
 		}
 
 		go func() {
 			for {
 				metrices.Getinfo()
+				metrices.Cpu()
 				updateMonitoringData()
 				time.Sleep(5 * time.Second)
 			}
@@ -56,23 +60,35 @@ type AllData struct {
 
 func handleData(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "application/json")
-	json.NewEncoder(w).Encode(allData)
+
+	// If displayedNodes is empty, send allData directly
+	if len(displayedNodes) == 0 {
+		json.NewEncoder(w).Encode(allData)
+		return
+	}
+
+	// Filter out the nodes that have already been displayed
+	newNodes := make([]metrices.NodeInfo, 0)
+	for _, node := range allData.NodeInfoList {
+		if !displayedNodes[node.Name[0]] {
+			newNodes = append(newNodes, node)
+			displayedNodes[node.Name[0]] = true
+		}
+	}
+
+	// Send only the new nodes in the response
+	newData := AllData{
+		Monitoring:   allData.Monitoring,
+		NodeInfoList: newNodes,
+	}
+	json.NewEncoder(w).Encode(newData)
 }
 
 func updateMonitoringData() {
 	// Simulate updating the monitoring data with new values
 	allData.Monitoring.Cpu = metrices.M.Cpu
 	allData.Monitoring.Usedmemory = metrices.M.Usedmemory
+
+	// Update the NodeInfoList with the latest data
+	allData.NodeInfoList = metrices.NodeInfoList
 }
-
-// func updateNodeInfo() {
-// 	// Simulate updating the node information with new values
-// 	for i := range allData.NodeInfoList {
-// 		allData.NodeInfoList[i].CPU = randFloat(1, 2)
-// 		allData.NodeInfoList[i].Memory = randFloat(10, 20)
-// 	}
-// }
-
-// func randFloat(min, max float64) float64 {
-// 	return min + rand.Float64()*(max-min)
-// }
